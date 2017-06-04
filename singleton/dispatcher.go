5c2b981be0f173c59f8af8fc0525d4688956378d
savaki <matt.ho@gmail.com>
2017-06-03 18:57:11 -0700

    - updated singleton dispatcher interface to match Repository shape

--- singleton/dispatcher.go
+++ singleton/dispatcher.go
@@ -9,13 +9,13 @@ import (
 // Dispatcher represents a function to execute a command
 type Dispatcher interface {
 	// Dispatch calls the command using the repository associated with the dispatcher
-	Dispatch(ctx context.Context, command eventsource.Command) (int, error)
+	Dispatch(ctx context.Context, command eventsource.Command) error
 }
 
 // DispatcherFunc provides a convenience func form of Dispatcher
-type DispatcherFunc func(ctx context.Context, command eventsource.Command) (int, error)
+type DispatcherFunc func(ctx context.Context, command eventsource.Command) error
 
 // Dispatch satisfies the Dispatcher interface
-func (fn DispatcherFunc) Dispatch(ctx context.Context, command eventsource.Command) (int, error) {
+func (fn DispatcherFunc) Dispatch(ctx context.Context, command eventsource.Command) error {
 	return fn(ctx, command)
 }
