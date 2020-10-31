740de3d1b953904fe71c684579b8b0775306c2df
Vance Longwill <vancelongwill@gmail.com>
2020-10-31 21:32:59 +0300

    adds generic protobuf serializer

--- go.mod
+++ go.mod
@@ -7,9 +7,11 @@ require (
 	github.com/aws/aws-sdk-go v1.35.19
 	github.com/boltdb/bolt v1.3.1
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang/protobuf v1.4.1
 	github.com/lib/pq v1.8.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.6.1
 	github.com/tj/assert v0.0.3 // indirect
+	google.golang.org/protobuf v1.25.0
 	gopkg.in/urfave/cli.v1 v1.20.0
 )
--- serializer_test.go
+++ serializer_test.go
@@ -2,9 +2,13 @@ package eventsource_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/vancelongwill/eventsource"
+	"github.com/vancelongwill/eventsource/pbevent"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type EntitySetName struct {
@@ -54,4 +58,44 @@ func TestJSONSerializer_MarshalAll(t *testing.T) {
 	found, ok := v.(*EntitySetName)
 	assert.True(t, ok)
 	assert.Equal(t, &event, found)
+}
+
+type UserCreatedEvent struct {
+	// used here as we dont have any other proto messages for testing purposes.
+	// checkout ./_examples/fullproto for a real working example
+	*pbevent.EventMeta
+}
+
+func (u UserCreatedEvent) AggregateID() string {
+	return u.Id
+}
+
+func (u UserCreatedEvent) EventVersion() int {
+	return int(u.Version)
+}
+
+func (u UserCreatedEvent) EventAt() time.Time {
+	return u.At.AsTime()
+}
+
+func TestProtoSerializer(t *testing.T) {
+	createdAt := timestamppb.Now()
+	event := UserCreatedEvent{
+		&pbevent.EventMeta{
+			Id:      "123",
+			Version: 1,
+			At:      createdAt,
+		},
+	}
+	serializer := eventsource.NewProtoSerializer(event)
+	record, err := serializer.MarshalEvent(event)
+	assert.Nil(t, err)
+	assert.NotNil(t, record)
+
+	v, err := serializer.UnmarshalEvent(record)
+	assert.Nil(t, err)
+
+	found, ok := v.(*UserCreatedEvent)
+	assert.True(t, ok)
+	assert.True(t, proto.Equal(&event, found))
 }
